f5c3328584f236b3e9b9bdef96a58593a28e24a5
moonydog12 <losididog@gmail.com>
2023-06-24 17:11:24 +0800

    Hadnling Errors

--- UsePopcorn/src/App.tsx
+++ UsePopcorn/src/App.tsx
@@ -3,15 +3,43 @@ import { Navbar, Logo, Search, NumResults } from './components/Navbar'
 import Box from './components/Box'
 import { MovieList } from './components/ListBox'
 import { WatchedMoviesList, WatchedSummary } from './components/WatchedBox'
-import { useState, ReactNode } from 'react'
+import { useState, ReactNode, useEffect } from 'react'
 
 interface Props {
   children: ReactNode
 }
 
+const KEY = '165606b8'
+const query = 'Dark Knight'
+
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData)
   const [watched, setWatched] = useState(tempWatchedData)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  // code that will run at different moments:mount, re-render, unmount
+  useEffect(
+    () => {
+      async function fetchData() {
+        try {
+          setIsLoading(true)
+          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
+          if (!res.ok) throw new Error('Something went wrong with fetching movie')
+          const data = await res.json()
+          if (data.Response === 'False') throw new Error('Movie not found')
+          setMovies(data.Search)
+        } catch (error) {
+          setError(error.message)
+        } finally {
+          setIsLoading(false)
+        }
+      }
+      fetchData()
+    },
+    // dependency array
+    []
+  )
 
   return (
     <>
@@ -23,7 +51,9 @@ export default function App() {
       </Navbar>
       <Main>
         <Box>
-          <MovieList movies={movies} />
+          {isLoading && <Loader />}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {error && <ErrorMessage message={error} />}
         </Box>
         <Box>
           <WatchedMoviesList watched={watched} />
@@ -36,4 +66,17 @@ export default function App() {
 
 function Main({ children }: Props) {
   return <main className="main">{children}</main>
+}
+
+function Loader() {
+  return <p className="loader">Loading</p>
+}
+
+function ErrorMessage({ message }) {
+  return (
+    <p className="error">
+      <span>❌</span>
+      {message}
+    </p>
+  )
 }
--- UsePopcorn/src/main.tsx
+++ UsePopcorn/src/main.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
+import App from './App.tsx'
 import Expander from './challenges/Expander.jsx'
 import StarRating from './components/StarRating.tsx'
-// import './index.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <StarRating
       maxRating={5}
       messages={['Terrible', 'Bad', 'OK', 'Good', 'Amazing']}
     />
     <StarRating size={24} defaultRating={3} color="blue" /> */}
-    <Expander />
+    {/* <Expander /> */}
   </React.StrictMode>
 )

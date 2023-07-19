7c020f49de744b21b504d112ae57671335943aab
moonydog12 <losididog@gmail.com>
2023-07-19 10:33:20 +0800

    Building Pages

--- WorldWise/src/App.jsx
+++ WorldWise/src/App.jsx
@@ -3,6 +3,8 @@ import Product from './pages/Product';
 import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing';
 import NotFound from './pages/NotFound';
+import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="app" element={<AppLayout />}></Route>
 
         {/* catching route */}
         <Route path="*" element={<NotFound />}></Route>

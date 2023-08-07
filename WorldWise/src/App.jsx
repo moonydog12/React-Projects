12b7260a7e0c31ba1316d261e82e8fa682a75c45
moonydog12 <losididog@gmail.com>
2023-08-07 20:28:24 +0800

    Add citiesContext

--- WorldWise/src/App.jsx
+++ WorldWise/src/App.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-
 import Product from './pages/Product';
 import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing';
@@ -11,61 +9,36 @@ import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
-
-const BASE_URL = 'http://localhost:8000';
+import { CitiesProvider } from './contexts/CitiesContext';
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  // fetching data on mount
-  useEffect(() => {
-    const fetchCities = async () => {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch (error) {
-        alert('Error loading data');
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchCities();
-  }, []);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />}></Route>
-        <Route path="product" element={<Product />}></Route>
-        <Route path="pricing" element={<Pricing />}></Route>
-        <Route path="login" element={<Login />}></Route>
-        <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate to="cities" replace />}></Route>
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />}></Route>
+          <Route path="product" element={<Product />}></Route>
+          <Route path="pricing" element={<Pricing />}></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate to="cities" replace />}></Route>
 
-          {/* nested route */}
-          {/* ex. app/cities */}
-          <Route
-            path="cities"
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          ></Route>
+            {/* nested route */}
+            {/* ex. app/cities */}
+            <Route path="cities" element={<CityList />}></Route>
 
-          {/* dynamic routes : routes with url parameters */}
-          <Route path="cities/:id" element={<City />}></Route>
+            {/* dynamic routes : routes with url parameters */}
+            <Route path="cities/:id" element={<City />}></Route>
 
-          <Route
-            path="countries"
-            element={<CountryList cities={cities} isLoading={isLoading} />}
-          ></Route>
-          <Route path="form" element={<Form />}></Route>
-        </Route>
+            <Route path="countries" element={<CountryList />}></Route>
+            <Route path="form" element={<Form />}></Route>
+          </Route>
 
-        {/* catching route */}
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
+          {/* catching route */}
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 }
 

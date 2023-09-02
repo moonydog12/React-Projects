73300e524041bece796f3aea0e86ced2faa37213
moonydog12 <losididog@gmail.com>
2023-09-02 20:55:14 +0800

    Reading and updating the global user state
    Modeling the 'user' state with redux toolkit

--- fast-react-pizza/src/features/users/userSlice.ts
+++ fast-react-pizza/src/features/users/userSlice.ts
@@ -1,3 +1,4 @@
+import { createSlice } from '@reduxjs/toolkit'
 import { getAddress } from '../../ui/services/apiGeocoding'
 
 function getPosition() {
@@ -21,3 +22,18 @@ async function fetchAddress() {
   // 3) Then we return an object with the data that we are interested in
   return { position, address }
 }
+
+const initialState = { username: '' }
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload
+    },
+  },
+})
+
+export const { updateName } = userSlice.actions
+export default userSlice.reducer

f731fe5d1ff74ab3782d15ae17ffccd3fd1f1126
lsd <lsd@ehk.com.tw>
2023-09-06 17:19:49 +0800

    Setting global styles & reusable components
    Import styled components library & using some functions it provides

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,5 +1,46 @@
+import { styled } from 'styled-components'
+import GlobalStyles from './styles/GlobalStyles'
+import Button from './ui/Button'
+import Input from './ui/Input'
+import Heading from './ui/Heading'
+import Row from './ui/Row'
+
+const StyledApp = styled.div`
+  padding: 20px;
+`
+
 function App() {
-  return <div></div>
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button variation="primary" size="medium">
+                Medium
+              </Button>
+
+              <Button variation="secondary" size="small">
+                Small
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form action="">
+              <Input type="number" placeholder="number of guests"></Input>
+              <Input type="number" placeholder="number of guests"></Input>
+              <Input type="number" placeholder="number of guests"></Input>
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  )
 }
 
 export default App
